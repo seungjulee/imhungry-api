234c7ee43e78a87691505ba56077193a88b3b10e
SJ Lee <seungju.sj.lee@gmail.com>
2016-02-22 04:08:50 -0800

    Fixed 500 error on input get req params. Added X-Origin workaround. Working But Slow

--- post_request.py
+++ post_request.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, jsonify, make_response, request, current_app
 app = Flask(__name__)
-
+app.debug = True
 import argparse
 import json
 import pprint
@@ -12,6 +12,10 @@ import requests
 import os
 from lxml import html
 from bs4 import BeautifulSoup
+
+# Util for X-Origin
+from datetime import timedelta
+from functools import update_wrapper
 
 API_HOST = 'api.yelp.com'
 DEFAULT_TERM = 'dinner'
@@ -25,26 +29,7 @@ PHOTO_BOX_PATH = 'http://www.yelp.com/biz_photos/'
 
 CREDENTIAL_FILE = 'credentials.json'
 
-# # OAuth credential placeholders that must be filled in by users.
-# CONSUMER_KEY = 'F893yhIehfbHx23uSzL9lA'
-# CONSUMER_SECRET = 'RQsbAEOrogv3_yc9qmnqtwcfKnI'
-# TOKEN = 'L6qh_QT420HYQtKVeCs5jTgRKiBA9R8r'
-# TOKEN_SECRET = 'r9BC272CDIC_ZexQ46_5GXIIgd0'
-
-# def save_credentials():
-#     credentials = {}
-#     credentials['CONSUMER_KEY'] = 'F893yhIehfbHx23uSzL9lA'
-#     credentials['CONSUMER_SECRET'] = 'RQsbAEOrogv3_yc9qmnqtwcfKnI'
-#     credentials['TOKEN'] = 'L6qh_QT420HYQtKVeCs5jTgRKiBA9R8r'
-#     credentials['TOKEN_SECRET'] = 'r9BC272CDIC_ZexQ46_5GXIIgd0'
-
-#     credentials_file_path = os.path.join(os.getcwd(), 'credentials.json')
-#     print(credentials_file_path)
-
-#     with open('credentials.json', 'w') as outfile:
-#         json.dump(credentials, outfile)
-
-def request(credentials, host, path, url_params=None):
+def make_request(credentials, host, path, url_params=None):
     """Prepares OAuth authentication and sends the request to the API.
     Args:
         host (str): The domain host of the API.
@@ -71,10 +56,10 @@ def request(credentials, host, path, url_params=None):
             'oauth_token': str(credentials['TOKEN']),
             'oauth_consumer_key': str(credentials['CONSUMER_KEY'])
             # 'oauth_token': TOKEN,
-            # 'oauth_consumer_key': CONSUMER_KEY  
+            # 'oauth_consumer_key': CONSUMER_KEY
         }
     )
-                        
+
     #token = oauth2.Token(TOKEN, TOKEN_SECRET)
     token = oauth2.Token(str(credentials['TOKEN']), str(credentials['TOKEN_SECRET']))
     oauth_request.sign_request(
@@ -103,7 +88,7 @@ def search(credentials, term, location):
         'location': location.replace(' ', '+'),
         'limit': SEARCH_LIMIT
     }
-    return request(credentials, API_HOST, SEARCH_PATH, url_params=url_params)
+    return make_request(credentials, API_HOST, SEARCH_PATH, url_params=url_params)
 
 def get_business(credentials, business_id):
     """Query the Business API by a business ID.
@@ -114,10 +99,15 @@ def get_business(credentials, business_id):
     """
     business_path = BUSINESS_PATH + business_id
 
-    return request(credentials, API_HOST, business_path)
+    return make_request(credentials, API_HOST, business_path)
 
 def get_photo_box_images(url_prefix, business_id, PHOTO_LIMIT):
-    photo_box_url = ''.join([url_prefix, business_id])
+    #TODO: change filter to generically get data from parameter
+    # food,inside,outside,drink,menu
+    PIC_FILTER = '?tab=food'
+
+    photo_box_url = ''.join([url_prefix, business_id, PIC_FILTER])
+    print photo_box_url, "pic box"
     page = requests.get(photo_box_url)
     if not page:
         print (u'No photo box page for {0} found.'.format(business_id))
@@ -191,7 +181,7 @@ def query_api(credentials, term, location):
 
     data['businesses'] = records
 
-    return json.dumps(data)  
+    return json.dumps(data)
 
 # def jsonify_business_data(business, photo_urls, error_code):
 #     data = {}
@@ -226,7 +216,7 @@ def get_credentials(credential_file_path):
     credentials = {}
 
     try:
-        with open(credential_file_path) as data_file:    
+        with open(credential_file_path) as data_file:
             credentials = json.load(data_file)
         f.close()
     except IOError:
@@ -234,26 +224,63 @@ def get_credentials(credential_file_path):
         sys.exit("Could not read file:", credential_file_path)
     finally:
         return credentials, error_code
-               
-#@app.route("/magicpage", methods=["GET", "POST"])
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 @app.route("/", methods=["GET"])
+@crossdomain(origin='*')
 def main():
-    #args = request.args
+    with open('credentials.json') as data_file:
+        credentials = json.load(data_file)
 
-    # if request.args.get("term", None) is not None:
-    #     print("yes")
-    # else:
-    #     print("no")
-
-    [credentials, error_code] = get_credentials(os.path.join(os.getcwd(), CREDENTIAL_FILE))
-    # save_credentials()
+    error_code = None
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-q', '--term', dest='term', default=DEFAULT_TERM,
+    parser.add_argument('-q', '--term', dest='term', default=request.args.get("term", None),
                         type=str, help='Search term (default: %(default)s)')
     parser.add_argument('-l', '--location', dest='location',
-                        default=DEFAULT_LOCATION, type=str,
+                        default=request.args.get("location", None), type=str,
                         help='Search location (default: %(default)s)')
     input_values = parser.parse_args()
 
@@ -262,7 +289,7 @@ def main():
 
     except urllib2.HTTPError as error:
         error_code = error.code
-        sys.exit('Encountered HTTP error {0}. Abort program.'.format(error.code)) 
+        sys.exit('Encountered HTTP error {0}. Abort program.'.format(error.code))
     finally:
         return json_data, error_code
 
